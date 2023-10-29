dc616951221eb0501e356fdc46a120b28735ed5d
HRLBZE <1214899196@qq.com>
2023-10-29 19:10:48 +0800

    10-29

--- Minesweeper/minesweeper.cpp
+++ Minesweeper/minesweeper.cpp
@@ -41,8 +41,142 @@ void input_bomb(int* bomb, int length, int width)//输入炸弹
 	} while (*bomb > (length * width));
 }
 
-void Mine_sweeper_str(char str[][128], int length, int width, int bomb)//分配1-9
+void Derm(int length, int width, char str[][128], char str1[][128])
 {
+	int length1, width1;
+	for (length1 = 0; length1 < length; length1++)
+	{
+		for (width1 = 0; width1 < width; width1++)
+		{
+			str1[length1][width1] = str[length1][width1];
+			if (str[length1][width1] == '1')
+				str1[length1][width1] = str1[length1][width1] - '1' + 'a';
+		}
+	}
+	for (length1 = 0; length1 < length; length1++)
+	{
+		for (width1 = 0; width1 < width; width1++)
+		{
+			if (str[length1][width1] == '0')
+			{
+				if (length1 == 0 && width1 == 0)//左上
+				{
+					if (str[length1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 == 0 && (width1 != 0 && width1 != width - 1))//上
+				{
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 == 0 && width1 == width - 1) //右上
+				{
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+				}
+				if ((length1 != 0 && length1 != length - 1) && width1 == width - 1)//右
+				{
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 == length - 1 && width1 == 0) //左下
+				{
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 + 1] == '1')
+						str1[length1][width1]++;
+				}
+				if ((length1 != 0 && length1 != length - 1) && width1 == 0)//左
+				{
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 == length - 1 && width1 == width - 1) //右下
+				{
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 == length - 1 && (width1 != 0 && width1 != width - 1))//下
+				{
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+				}
+				if (length1 != 0 && length1 != length - 1 && width1 != 0 && width1 != width - 1)
+				{
+					if (str[length1 - 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 - 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 - 1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1] == '1')
+						str1[length1][width1]++;
+					if (str[length1 + 1][width1 + 1] == '1')
+						str1[length1][width1]++;
+				}
+			}
+		}
+	}
+}
+
+void Mine_sweeper_str(char str[][128], int length, int width, int bomb, char str1[][128])//分配1-9
+{
+	int length1 = 0;
+	int width1 = 0;
 	srand(time(NULL));
 	//num=a+(b-a+1)*rand()/(RAND-MAX+1.0)；
 	for (; bomb > 0; bomb--)
@@ -74,25 +208,30 @@ void Mine_sweeper_str(char str[][128], int length, int width, int bomb)//分配1-9
 			}
 		}
 	}
+	for (; length1 < length; length1++)
+	{
+		for (width1 = 0; width1 < width; width1++)
+		{
+			if (str[length1][width1] != '0')
+				str[length1][width1] = '1';
+			else
+				continue;
+		}
+
+	}
+	Derm(length, width, str, str1);
 }
 
-typedef struct
-{
-	IMAGE picture;
-	double x;
-	double y;
-}photos;
-
-void loadphoto(photos* a, int i)//载入图画
+void load_photo(IMAGE* A, int i)	//载入图片
 {
 	char name[64];
 	sprintf(name, "picture/%d.png", i);
-	loadimage(&a->picture, name, 50, 50);
+	loadimage(A, name, 50, 50);
 }
 
-void putphoto(photos* a, int weight, int height)//放出图画
+void put_photo(IMAGE A, int i, int weight_w, int height_h)	//放出图片
 {
-	putimage(weight, height, &a->picture);
+	putimage(weight_w, height_h, &A);
 }
 
 void init(int width, int length)
@@ -100,17 +239,62 @@ void init(int width, int length)
 	initgraph(width * 50, length * 50, NULL);
 }
 
+void Mouse(ExMessage& m, ExMessage& n, char str[128][128], IMAGE A[10])
+{
+	printf("if you want to exit,please click the mbuttondown");
+	do
+	{
+		m = n;
+		getmessage(&m, EM_MOUSE);
+		if (m.message == WM_LBUTTONDOWN)
+		{
+			int c;
+			int m_x1;
+			int m_y1;
+			m_x1 = m.x / 50;
+			m_y1 = m.y / 50;
+			str[m_y1][m_x1]++;
+			if (str[m_y1][m_x1] > '9')
+			{
+				str[m_y1][m_x1] = '9';
+			}
+			c = str[m_y1][m_x1] - '0';
+			put_photo(A[c], c, m_x1 * 50, m_y1 * 50);
+		}
+		if (m.message == WM_RBUTTONDOWN)
+		{
+			int c;
+			int m_x1;
+			int m_y1;
+			m_x1 = m.x / 50;
+			m_y1 = m.y / 50;
+			str[m_y1][m_x1]--;
+			if (str[m_y1][m_x1] < '0')
+			{
+				str[m_y1][m_x1] = '0';
+			}
+			c = str[m_y1][m_x1] - '0';
+			put_photo(A[c], c, m_x1 * 50, m_y1 * 50);
+		}
+	} while (m.message != WM_MBUTTONDOWN);
+}
+
 int main()
 {
+	ExMessage m;
+	ExMessage n = { 0 };
+	int m_x;
+	int m_y;
 	int bomb, length, width;
 	int photo = 0;
 	int weight_w = 0;
 	int height_h = 0;
 	char str[128][128] = { 0 };
+	char str1[128][128] = { 0 };
 	input_length(&length);
 	input_width(&width);
 	input_bomb(&bomb, length, width);
-	Mine_sweeper_str(str, length, width, bomb);
+	Mine_sweeper_str(str, length, width, bomb, str1);
 	for (int i = 0; i < length; i++)
 	{
 		for (int j = 0; j < width; j++)
@@ -119,27 +303,29 @@ int main()
 		}
 		putchar('\n');
 	}
-	photos* pho[10];
-	for (int i = 0; i < 10; i++)
+	IMAGE A[11];
+	for (int i = 0; i < 11; i++)
 	{
-		pho[i] = new photos;
-		loadphoto(pho[i], i);
+		load_photo(&A[i], i);
 	}
 	init(width, length);
-
-	for (int i = 0; i < length; i++)//i is weight,j is height
+	for (int i = 0; i < length; i++)//i is h,j is w
 	{
 		for (int j = 0; j < width; j++)
 		{
-			int c = str[i][j] - '0';
-			putphoto(pho[c], weight_w, height_h);
+			int c = str1[i][j] - '0';
+			if (c == 'a' - '0')
+			{
+				put_photo(A[10], 10, weight_w, height_h);
+			}
+			else
+				put_photo(A[c], c, weight_w, height_h);
 			weight_w += 50;
 		}
 		height_h += 50;
 		weight_w = 0;
 	}
-	system("pause");
-	getchar();
+	Mouse(m, n, str, A);
 	closegraph();
 	return 0;
 }
